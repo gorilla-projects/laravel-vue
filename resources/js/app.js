02b50875f6aca17d8b0d017126b5bf6b41e661c5
Toby Versteeg <toby@codegorilla.nl>
2022-07-05 13:14:25 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+window.Vue = require('vue').default;
+
+Vue.config.productionTip = false;
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png  ">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <title>Document</title>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+    <div id="app">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
